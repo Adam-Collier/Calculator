d6e2bd83de4a2a16f5ce3e3ef3553b188c81d29a
Adam-Collier <adamcollierart@gmail.com>
2017-02-16 20:42:43 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+  var arr = [];
+$("button").on("click", function(){
+  if($(this).html() === "C"){
+    $("h2").html("");
+    $("p").html("");
+    arr = [];
+  }else if($(this).html() === " + " || $(this).html() === " x " || $(this).html() === " - " || $(this).html() === " ÷ "){
+    arr.push($("h2").html());
+    arr.push($(this).html());
+    $("p").append($("h2").html());
+    $("p").append($(this).html());
+    $("h2").html("");
+  }else if($(this).html() === "="){
+    arr.push($("h2").html());
+    arr = arr.map(function(x){
+      return x.trim(" ");
+    });
+    var a = arr.join("").replace("x", '*').replace("÷", "/");
+    arr = [];
+    var e = eval(a);
+    $("p").html("");
+    $("h2").html(e.toString());
+  }
+  else{
+    $("h2").append($(this).html());
+  }
+});
+
+});
